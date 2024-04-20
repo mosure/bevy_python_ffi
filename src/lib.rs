f452d3da937b90db67f936a1a263132c415a1ba2
mosure <mitchell@mosure.me>
2024-04-20 11:06:17 -0500

    feat: setup.py support

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 
 // TODO: setup application cache which can be used to process messages and add into app world
-// TODO: add pyproject build-tools setup instead of maturin
 
 // fn setup_and_run_app(app: App)...
 
